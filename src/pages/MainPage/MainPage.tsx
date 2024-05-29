ad726fddd4fa2d658120104e89770dff665b3ce9
Macie Kluting <macie.kluting@gmail.com>
2024-05-28 23:32:31 -0500

    Extra :: add forced error state checkbox

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,20 +5,27 @@ import { Package } from "./types";
 
 export function MainPage() {
   const [loading, setLoading] = useState(false);
-  const [showError, setShowError] = useState(false);
+  const [forceError, setForceError] = useState(false);
+  const [showErrorMessage, setShowErrorMessage] = useState(false);
   const [searchString, setSearchString] = useState("");
   const [packages, setPackages] = useState<Package[]>([]);
 
   const getNpmPackages = async (queryString: string) => {
     setLoading(true);
+    setShowErrorMessage(false);
 
     try {
       const { data } = await axios.get(
         `https://api.npms.io/v2/search/suggestions?q=${queryString}`,
       );
-      setPackages(data);
+
+      if (forceError) {
+        throw new Error();
+      } else {
+        setPackages(data);
+      }
     } catch {
-      setShowError(true);
+      setShowErrorMessage(true);
     } finally {
       setLoading(false);
     }
@@ -38,27 +45,38 @@ export function MainPage() {
           }}
         />
         <button onClick={() => getNpmPackages(searchString)}>Search</button>
+        <div className="error-toggle">
+          <input
+            type="checkbox"
+            id="forceError"
+            name="forceError"
+            value="forceError"
+            checked={forceError}
+            onChange={(event) => setForceError(event.target.checked)}
+          />
+          <label htmlFor="forceError">Test Error State</label>
+        </div>
       </div>
 
       {loading ? (
         <span>Loading...</span>
       ) : (
         <>
-          {showError && (
+          {showErrorMessage ? (
             <span>Oh no! Something went wrong... please try again.</span>
-          )}
-
-          {packages.length > 0 && (
-            <div className="results">
-              {packages.map((pkg) => (
-                <div className="result-item" key={pkg.package.name}>
-                  <strong>
-                    <a href={pkg.package.links.npm}>{pkg.package.name}</a>
-                  </strong>
-                  <span>{pkg.package.description}</span>
-                </div>
-              ))}
-            </div>
+          ) : (
+            packages.length > 0 && (
+              <div className="results">
+                {packages.map((pkg) => (
+                  <div className="result-item" key={pkg.package.name}>
+                    <strong>
+                      <a href={pkg.package.links.npm}>{pkg.package.name}</a>
+                    </strong>
+                    <span>{pkg.package.description}</span>
+                  </div>
+                ))}
+              </div>
+            )
           )}
         </>
       )}
